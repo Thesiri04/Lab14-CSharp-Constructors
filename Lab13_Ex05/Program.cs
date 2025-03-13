613e9e11073420cf1ba3bb407666173b4342f6f6
Thesiri04 <sirirungnapa201@gmail.com>
2025-03-13 20:39:16 +0700

    แบบฝึก

---
+++ Lab13_Ex05/Program.cs
@@ -0,0 +1,49 @@
+﻿class Shape
+{
+    private int? NumOfSide;
+
+    // เปลี่ยนจาก private เป็น protected
+    protected Shape()
+    {
+    }
+
+    public Shape(int NumOfSide) : this()
+    {
+        this.NumOfSide = NumOfSide;
+        System.Console.WriteLine($"This is some shape with {NumOfSide} sides");
+    }
+}
+
+class Circle : Shape
+{
+    public Circle() : base(0)
+    {
+        System.Console.WriteLine("This is a circle");
+    }
+}
+
+class Rectangle : Shape
+{
+    public Rectangle() : base(4)
+    {
+        System.Console.WriteLine("This is a rectangle");
+    }
+}
+
+class Triangle : Shape
+{
+   public Triangle() : base(3) // แก้ไขให้มีการส่งค่าเป็น 3
+    {
+        System.Console.WriteLine("This is a triangle");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var circle = new Circle();
+        var rectangle = new Rectangle();
+        var triangle = new Triangle();
+    }
+}
